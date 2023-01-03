a70f673f7d787e2141922d8bc67972861e127436
Kadir Cetin <kcetin@k1m05s05.42istanbul.com.tr>
2023-01-03 11:19:24 +0300

    ok

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -1,45 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcetin <kcetin@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 07:53:31 by kcetin            #+#    #+#             */
+/*   Updated: 2023/01/03 11:12:04 by kcetin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "phonebook.hpp"
-
-void search()
-{
-	std::cout << "┌──────────┬──────────┬──────────┬──────────┐" << std::endl;
-    std::cout << "│" << std::setw(10) << std::right << "index";
-    std::cout << "│" << std::setw(10) << std::right << "first name";
-    std::cout << "│" << std::setw(10) << std::right << "last name";
-    std::cout << "│" << std::setw(10) << std::right << "nickname" << "│" << std::endl;
-    std::cout << "├──────────┼──────────┼──────────┼──────────┤" << std::endl;
-}
-
-
 
 int main()
 {
     PhoneBook phonebook;
-    std::string	str;
+    std::string	str = ".";
     int id = -1;
     phonebook.prompt();
     
-    while(42)
+	while (str != "EXIT")
     {
         std::cout << "> ";
         std::getline(std::cin, str);
         if(str == "ADD")
-            std::cout << "ADD" << std::endl;
+            phonebook.add();
         else if(str == "SEARCH")
         {
+            phonebook.users();
+            std::cout << "enter number" << std::endl;
             if (!(std::cin >> id) || id < 0 || id > 7)
 			{
 				std::cin.clear();
 				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 				std::cout << "Error: Invalid ID" << std::endl;
 			}
-        }
-        else if(str == "EXIT")
-            return 0;
-        else
-        {
-            std::cout << "Wrong word try again" << std::endl;
-            phonebook.prompt();
+            else
+                phonebook.display(id);
         }
     }
 }
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcetin <kcetin@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 07:54:14 by kcetin            #+#    #+#             */
+/*   Updated: 2023/01/03 11:14:53 by kcetin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
@@ -7,25 +19,100 @@
 # include <cstring>
 # include <limits>
 
+class Contact{
+public:
+	bool set_info(int last_id)
+	{
+		std::cout << "Enter the first name: " << std::endl;
+		std::getline(std::cin, info[0]);
+		std::cout << "Enter the last name: " << std::endl;
+		std::getline(std::cin, info[1]);
+		std::cout << "Enter the nickname: " << std::endl;
+		std::getline(std::cin, info[2]);
+		std::cout << "Enter the phone number: " << std::endl;
+		std::getline(std::cin, info[3]);
+		std::cout << "Enter the darkest secret: " << std::endl;
+		std::getline(std::cin, info[4]);;
+		for (int i = 0; i < 5; i++)
+			if (info[i] == "")
+			{
+				std::cout << "Empty field in contact data. Unable to add." << std::endl;
+				return (false);
+			}
+		id = last_id;
+		return (true);
+	}
+	const std::string* get_info() { return info; }
+private:
+	int id;
+	std::string info[5];
+};
+
 class PhoneBook{
 public:
     size_t  person_number;
     std::string persons[8];
 
+	PhoneBook()
+	{
+		last_id = 0;
+	}
+
     void prompt(void)
     {
-		std::cout << "Welcome to PhoneBook" << std::endl;
+		std::cout << std::endl <<"Welcome to PhoneBook" << std::endl;
     	std::cout << "ADD    : Add user to the book" << std::endl;
     	std::cout << "SEARCH : Looking for somebody" << std::endl;
     	std::cout << "EXIT   : Goodbye" << std::endl;
     }
 
+	void add()
+	{
+		Contact new_contact;
+		if(new_contact.set_info(last_id))
+		{
+			if (contact[last_id].get_info()[0] != "")
+				std::cout << "limit is full" << std::endl;
+			contact[last_id] = new_contact;
+			last_id  = (last_id + 1) % 8;
+		}
+		prompt();
+	}
+
+	void users()
+	{
+		for (int i = 0; i < 8; i++) 
+		{
+			if (contact[i].get_info()[0] != "")
+			{
+				std::cout << "|" << std::setw(10) << i;
+				for (int j = 0; j < 3; j++)
+				{
+					std::cout << "|";
+					if (contact[i].get_info()[j].length() > 10)
+						std::cout << contact[i].get_info()[j].substr(0, 9) << ".";
+					else
+						std::cout << std::setw(10) << contact[i].get_info()[j];
+					}
+					std::cout << "|" << std::endl;
+			}
+		}
+	}
+
+	void display(int id)
+	{
+		if (contact[id].get_info()[0] != "") {
+			std::cout << "Contact info:" << std::endl;
+			std::cout << id << std::endl;
+			for (int j = 0; j < 5; j++)
+				std::cout << contact[id].get_info()[j] << std::endl;
+		}
+		else
+			std::cout << "Error: Invalid ID" << std::endl;
+	}
 private:
-
-};
-
-class Contact{
-
+	Contact contact[8];
+	int last_id;
 };
 
 #endif
